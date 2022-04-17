7ea970f1be60c913738fd070533ac6b0c62d1096
Ar Rakin <rakinar2@gmail.com>
2022-04-17 20:02:20 +0600

    Fix node crash in guard.js

--- src/guard.js
+++ src/guard.js
@@ -1,10 +1,8 @@
-const commands = require("./commands");
-
 const disallowedCommandsGlobal = [];
 const disallowedCommands = [];
 
 function isAllowedInChannel(cmd) {
-    return disallowedCommandsGlobal.indexOf(cmd) === -1 && (global.config.props.control_channel === '-' || global.commands.channel === global.config.props.control_channel || global.commands.msg.channel?.parent.id === global.config.props.channel_category);
+    return disallowedCommandsGlobal.indexOf(cmd) === -1 && (global.config.props.control_channel === '-' || global.commands.channel === global.config.props.control_channel || global.commands.msg.channel?.parent?.id === global.config.props.channel_category);
 }
 
 function isAllowed(cmd) {
